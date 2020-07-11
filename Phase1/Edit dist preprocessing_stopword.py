3dbfb037192a59702b31c118b12c32d98fac082c
palak740 <65844671+palak740@users.noreply.github.com>
2020-07-11 20:20:27 +0530

    Add files via upload

---
+++ Phase1/Edit dist preprocessing_stopword.py
@@ -0,0 +1,27 @@
+import nltk
+from nltk.tokenize import sent_tokenize,word_tokenize
+from nltk.corpus import stopwords
+l=["BS19B002_Q1.txt","BS19B002_Q2.txt","BS19B002_Q3.txt","BS19B009_Q1.txt","BS19B009_Q2.txt","BS19B009_Q3.txt","BS19B012_Q1.txt","BS19B012_Q2.txt","BS19B012_Q3.txt","BS19B014_Q1.txt","BS19B014_Q2.txt","BS19B014_Q3.txt","BS19B020_Q1.txt","BS19B020_Q2.txt","BS19B020_Q3.txt","BS19B022_Q1.txt","BS19B022_Q2.txt","BS19B022_Q3.txt","BS19B023_Q1.txt","BS19B023_Q2.txt","BS19B023_Q3.txt","BS19B025_Q1.txt","BS19B025_Q2.txt","BS19B025_Q3.txt","BS19B028_Q1.txt","BS19B028_Q2.txt","BS19B028_Q3.txt","BS19B029_Q1.txt","BS19B029_Q2.txt","BS19B029_Q3.txt","BS19B030_Q1.txt","BS19B030_Q2.txt","BS19B030_Q3.txt","BS19B031_Q1.txt","BS19B031_Q2.txt","BS19B031_Q3.txt"]
+for i in l:
+    f=open(i,'r')
+    text=f.read()
+    text=text.lower()
+    text=word_tokenize(text)
+    sw=set(stopwords.words('english'))
+    t=[c for c in text if not c in sw]
+    t=" ".join(t)
+    t=sent_tokenize(t)
+    def punc(t):
+        punctuations = '''!()-[]{};:'"\,<>/?@#$%^&*~. =+-|`'''
+        for i in range(len(t)):
+            strb=''
+            for char in t[i]:
+                if char not in punctuations:
+                    strb=strb+char
+                else:
+                    strb=strb
+            t[i]=strb
+        return(t)
+    t=punc(t)
+    print(t,'\n\n')
+f.close()
